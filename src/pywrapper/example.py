bf5322e7a75030b458d5db445af7b0998055131c
SeafoodStudios <williamluqilin@gmail.com>
2025-05-13 18:28:05 -0400

    Update example.py

--- src/pywrapper/example.py
+++ src/pywrapper/example.py
@@ -1,10 +1,8 @@
-# To use this, download the WebDB Python class file (called main.py) into the folder you are using.
-# Rename main.py into webdb.py
+# To use this, download the WebDB Python class file (called webdb.py) into the folder you are using.
 
 from webdb import WebDB
-
 webdb = WebDB()
 webdb.create("variable", "password", "value")
 webdb.update("variable", "password", "newvalue")
-webdb.get("variable")
+print(str(webdb.get("variable")))
 webdb.delete("variable", "password")
