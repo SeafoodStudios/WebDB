5555105974a7aa610961017376dc5595c92540ac
SeafoodStudios <williamluqilin@gmail.com>
2025-05-13 10:55:22 -0400

    Create main.py

---
+++ src/pywrapper/main.py
@@ -0,0 +1,44 @@
+import requests
+
+class WebDB:
+    def create(self, variable, password, value):
+        create_payload = {
+            "variable": str(variable),
+            "password": str(password),
+            "value": str(value)
+        }
+        create_response = requests.post(
+            'https://webdb.pythonanywhere.com/create/',
+            headers={'Content-Type': 'application/json'},
+            json=create_payload
+        )
+    def delete(self, variable, password, value):
+        create_payload = {
+            "variable": str(variable),
+            "password": str(password),
+        }
+        create_response = requests.post(
+            'https://webdb.pythonanywhere.com/delete/',
+            headers={'Content-Type': 'application/json'},
+            json=create_payload
+        )
+    def update(self, variable, password, newvalue):
+        create_payload = {
+            "variable": str(variable),
+            "password": str(password),
+        }
+        create_response = requests.post(
+            'https://webdb.pythonanywhere.com/delete/',
+            headers={'Content-Type': 'application/json'},
+            json=create_payload
+        )
+        create_payload = {
+            "variable": str(variable),
+            "password": str(password),
+            "value": str(newvalue)
+        }
+        create_response = requests.post(
+            'https://webdb.pythonanywhere.com/create/',
+            headers={'Content-Type': 'application/json'},
+            json=create_payload
+        )
