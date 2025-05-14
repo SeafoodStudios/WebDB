abb72cd71dbba9d08353e79d7fa678c972440c78
SeafoodStudios <williamluqilin@gmail.com>
2025-05-14 08:56:58 -0400

    Create main.sh

---
+++ build/main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/SeafoodStudios/WebDB
+pip3 install flask
+pip3 install tinydb
+pip3 install bcrypt
+python3 WebDB/src/server/main.py
+python3 -m flask --app WebDB/src/server/main.py run
