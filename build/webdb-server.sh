6d5a280f5637012a773f44a2c20e054b23bfbaf2
SeafoodStudios <williamluqilin@gmail.com>
2025-05-23 13:29:25 -0400

    Update webdb-server.sh

--- build/webdb-server.sh
+++ build/webdb-server.sh
@@ -4,5 +4,6 @@ git clone https://github.com/SeafoodStudios/WebDB
 pip3 install flask
 pip3 install tinydb
 pip3 install bcrypt
+pip3 install flask_limiter
 python3 WebDB/src/server/main.py
 python3 -m flask --app WebDB/src/server/main.py run
